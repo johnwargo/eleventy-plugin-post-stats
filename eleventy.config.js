55bd23f2c2aae7b2ded68d86aa2a249203cb3a70
John M. Wargo <johnwargo@users.noreply.github.com>
2023-07-07 18:30:07 -0400

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,15 +2,15 @@ const postStats = require('./eleventy-plugin-post-stats.js');
 
 module.exports = eleventyConfig => {
 
-  eleventyConfig.addPlugin(postStats, {debugMode: true });
+	eleventyConfig.addPlugin(postStats, { debugMode: true });
 
-  return {
+	return {
 		dir: {
-      input: 'src',
+			input: 'src',
 			output: "_site",
 			includes: "_includes",
 			layouts: "_layouts",
-			data: "_data"			
+			data: "_data"
 		}
 	}
 };
